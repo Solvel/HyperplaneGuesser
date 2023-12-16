51386142f5843da0fe75789e4e22f5d6e3c55c93
Solvel <65024135+Solvel@users.noreply.github.com>
2023-12-16 20:32:41 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from perceptron_gradient_descent import PerceptronGradientDescent
 import pandas as pd
 from plotting import plot_mints
 
-def main(tries=1, perceptron_type=0):
+def main(runs=1, perceptron_type=0):
     # Specify the file path
     file_path = r'seperable_data_2d.csv'
 
@@ -13,19 +13,19 @@ def main(tries=1, perceptron_type=0):
 
     total_guesses = 0
 
-    for _ in range(tries):
+    for _ in range(runs):
         if perceptron_type == 0:
-            pl = Perceptron() # Average guesses over 1000 tries: 376.916
+            pl = Perceptron() # Average guesses over 1000 runs: 376.916
         elif perceptron_type == 1:
             pl = PerceptronGradientDescent() # Same 121 epochs every run
         pl.fit(df[['x1', 'x2']].to_numpy()[:80], df['y'].to_numpy()[:80])
         total_guesses += pl.counter if hasattr(pl, 'counter') else pl.epochs
 
-    average_guesses = total_guesses / tries
-    print(f"Average guesses over {tries} tries: {average_guesses}")
+    average_guesses = total_guesses / runs
+    print(f"Average guesses over {runs} runs: {average_guesses}")
 
     # Use the last trained perceptron for plotting
     plot_mints(df, pl)
 
 if __name__ == "__main__":
-    main(tries=1, perceptron_type=1)
+    main(runs=1, perceptron_type=1)
