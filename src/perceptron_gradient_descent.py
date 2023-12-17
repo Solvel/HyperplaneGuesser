772901eae8afb21841e3ab57cb5cbae314ea9048
Solvel <65024135+Solvel@users.noreply.github.com>
2023-12-17 16:00:46 +0100

    Update perceptron_gradient_descent.py

--- src/perceptron_gradient_descent.py
+++ src/perceptron_gradient_descent.py
@@ -4,17 +4,17 @@ import numpy as np
 class PerceptronGradientDescent:
     def __init__(self, learning_rate=0.01, max_iterations=1000):
         self.learning_rate = learning_rate
-        self.counter = 0  # Rename 'epochs' to 'counter'
+        self.counter = 0 
         self.weights = None
         self.bias = None  # Update the attribute name from bias to offset
-        self.max_iterations = max_iterations  # Rename 'epochs' to 'max_iterations'
+        self.max_iterations = max_iterations
 
     def fit(self, X, y):
         # Initialize weights and bias
         self.weights = np.zeros(X.shape[1])
         self.bias = 0.0
 
-        for self.counter in range(self.max_iterations):  # Rename 'epochs' to 'counter'
+        for self.counter in range(self.max_iterations):
             # Calculate the dot product of features and weights
             linear_output = np.dot(X, self.weights) + self.bias
 
